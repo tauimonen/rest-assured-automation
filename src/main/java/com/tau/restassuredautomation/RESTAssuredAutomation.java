dcc39a1b61c41ca527713a68f39ca90f44071c39
vgtoui <toni.uimonen@tuni.fi>
2023-10-27 18:01:38 +0300

    Create a maven project

---
+++ src/main/java/com/tau/restassuredautomation/RESTAssuredAutomation.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.tau.restassuredautomation;
+
+/**
+ *
+ * @author uimon
+ */
+public class RESTAssuredAutomation {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
